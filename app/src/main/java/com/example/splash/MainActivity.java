8d6df8de8a740bf5b715ec023b61bcb44a7c9f8c
Diana-gonzalez99 <6260@itescam.edu.mx>
2021-04-26 21:28:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.splash;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer time = new Timer();
+        time.schedule(tarea,9000);
+    }
+
+
+
+
+}
